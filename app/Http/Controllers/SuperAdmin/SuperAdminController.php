de7d8194c449b1fa34980c0f17984aeb34f46b32
Akshay Dhanwate <akshay.dhanwate@brainoidtech.in>
2026-05-07 16:46:04 +0530

    testing chnages

--- app/Http/Controllers/SuperAdmin/SuperAdminController.php
+++ app/Http/Controllers/SuperAdmin/SuperAdminController.php
@@ -37,9 +37,9 @@ class SuperAdminController extends Controller
 
             session()->forget('department_id');
 
-            $data['topmenu'] = 'Dashboard1';
-            $data['submenu'] = 'Dashboard1';
-            $data['pagetitle'] = 'Dashboard1';
+            $data['topmenu'] = 'Dashboard';
+            $data['submenu'] = 'Dashboard';
+            $data['pagetitle'] = 'Dashboard';
 
             $depts =  $this->dept->getDeptData($deptName);
             // for search dept
